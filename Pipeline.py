85705b98d41ba92309135964f0b9ac2521b8f862
Maxime-Skryma <maxime.skryma@gmail.com>
2026-08-28 08:45:41 +0000

    still on pipleing

--- Pipeline.py
+++ Pipeline.py
@@ -1,29 +1,93 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
-#===================================
-# 1st Figure : Evolution of Currencies
-locator = mdates.AutoDateLocator()
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
 
 
-fig,ax=plt.subplots(figsize=(12,5))
+def adf_manuel(log, p): #log est notre série, p est le nombre de lags
+    dlog = log.diff() #Δlog(X) il va notamment servir pour la variable à prédire, et les lags différenciés, mais c'est pas la composante principale
+    X = pd.DataFrame(index=log.index) #Notre tableau des valeurs de log(X)
+    X['niveau_lag1'] = log.shift(1) #On ajoute une colonne avec un lag 1 : ie Xt-1, ça sera ce sur quoi on régresse.
+    for i in range(1, p + 1):
+        X[f'dlog_lag_{i}'] = dlog.shift(i) #Ici on crée les lags différenciés à 'p niveau', Xt-2 - Xt-3 par exemple, Xt-4 - Xt-5
+    X['const'] = 1.0 #constante pour notre régression
+    reg = pd.concat([dlog.rename('dlog'), X], axis=1).dropna() #concaténation de X et de notre lag différencié (ce qu'on veut prédire)
+    res = sm.OLS(reg['dlog'], reg.drop(columns='dlog')).fit() #on fit notre régression : on prédit Δlog(Xt) à l'aide de log(X t-1), Δlog(Xt-1), Δlog(Xt-2)...
+    return res.tvalues['niveau_lag1'], res.aic, len(reg) #On demande d'avoir la t_value, uniquement du lag 1, c'est celle qui nous intéresse
 
-columns = pd.Series(prices.columns) #On en fait une Série
-columns = prices.columns[1:] #On enlève open_time
-print(columns)
 
-for col in columns:
-    ax.plot(prices['open_time'],
-    prices[col]/(prices[col].iloc[0])*100, #To be on the same scale, we compare their evolution, not their level
-    label=col,
-    alpha=0.8)
-ax.set_xlabel("Time")
-ax.set_ylabel("Normalized Prices in base 100")
-ax.legend()
-ax.xaxis.set_major_locator(locator)
-ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
-plt.show()
-    
-#===================================
-# 1st Figure : Evolution of Currencies
+def pipeline(df):
+    #===================================
+    # 1st Figure : Evolution of Currencies
+    locator = mdates.AutoDateLocator()
 
+
+    fig,ax=plt.subplots(figsize=(12,5))
+
+    columns = pd.Series(df.columns) #On en fait une Série
+    columns = df.columns[1:] #On enlève open_time
+    print(columns)
+
+    for col in columns:
+        ax.plot(df['open_time'],
+        df[col]/(df[col].iloc[0])*100, #To be on the same scale, we compare their evolution, not their level
+        label=col,
+        alpha=0.8)
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Normalized df in base 100")
+    ax.legend()
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
+    plt.show()
+    fig.savefig("Spread_Crypto/figs/crypto_df.png")
+        
+    #===================================
+    log_symbols=[]
+    for col in columns:
+        log_symbols.append(f'log_{col}')
+        df[f'log_{col}']=np.log(df[col])
+        
+    # 2nd Figure : Evolution of Log_Currencies
+    fig,ax=plt.subplots(figsize=(12,5))
+
+    for col in log_symbols:
+        ax.plot(df['open_time'],
+        df[col], #To be on the same scale, we compare their evolution, not their level
+        label=col,
+        alpha=0.8)
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Log df")
+    ax.legend()
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
+    plt.show()
+    fig.savefig("Spread_Crypto/figs/crypto_log_df.png")
+
+    #===================================================================
+    # Manual ADF and Automatic ADF on (log) - currencies
+
+    for ls in log_symbols:
+        aics = {}
+        log = df[ls]
+        print(f"\n=== {ls} ===")
+        for p in range(1, 16):
+            t, aic, n = adf_manuel(log, p) #boucle pour appliquer l'ADF manuel pour différents nombre de lags
+            aics[p] = aic #On remplit notre tableau d'AIC (pour pouvoir en tirer l'argmin notamment)
+            ref = adfuller(log.dropna(), maxlag=p, autolag=None, regression='c')[0] #On applique directement notre ADF automatique sur les colonnes log du df[]
+            print(f"p={p:2d}  t={t:7.3f}  refdf={ref:7.3f}  écart={abs(t-ref):.2e} AIC={aic:.2e} n={n}")
+        p_opt = min(aics, key=aics.get)
+        print(f'the number of diffs for {ls} that minimizes AIC is {p_opt}')
+
+    #===================================================================
+    # Manual ADF and Automatic ADF on Diff - (log) - currencies
+    for ls in log_symbols:
+        aics = {}
+        log = df[ls]
+        print(f"\n=== {ls} ===")
+        for p in range(1, 16):
+            t, aic, n = adf_manuel(log.diff(), p) #boucle pour appliquer l'ADF manuel pour différents nombre de lags
+            aics[p] = aic #On remplit notre tableau d'AIC (pour pouvoir en tirer l'argmin notamment)
+            ref = adfuller(log.dropna(), maxlag=p, autolag=None, regression='c')[0] #On applique directement notre ADF automatique sur les colonnes log du df[]
+            print(f"p={p:2d}  t={t:7.3f}  refdf={ref:7.3f}  écart={abs(t-ref):.2e} AIC={aic:.2e} n={n}")
+        p_opt = min(aics, key=aics.get)
+        print(f'the number of diffs for {ls} that minimizes AIC is {p_opt}')
