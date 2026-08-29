43ab9a1ed1ab5acc6836175484b64accf300c5e9
Maxime-Skryma <maxime.skryma@gmail.com>
2026-08-29 23:20:30 +0000

    still error

--- GARCH_pipeline.py
+++ GARCH_pipeline.py
@@ -7,7 +7,7 @@ from statsmodels.tsa.arima_process import ArmaProcess
 from scipy.optimize import minimize
 from scipy.special import gammaln
 
-
+from ARCH_GARCH import log_vraisemblance_GJR_GARCH_student,log_vraisemblance_GARCH
 
 
 def simulate_GJR_GARCH_student(n, alpha0, alpha1, beta0, v, y, loi):
