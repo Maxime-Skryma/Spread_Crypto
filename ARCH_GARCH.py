correction GARCH by correcting student law (we need variance=1)

ARCH_GARCH.py
@@ -284,7 +284,7 @@ def log_vraisemblance_GARCH_student(zeta, actif):
 def opti_AR_GARCH_student(log_vraisemblance_GARCH_student,parametres_initiaux,actif):
 
     resultat_optimisation = minimize(
-        fun=log_vraisemblance_GARCH, 
+        fun=log_vraisemblance_GARCH_student, 
         x0=parametres_initiaux, 
         args=(actif), 
         method='Nelder-Mead' # Algorithme robuste qui ne nécessite pas de gradient parfait
@@ -294,3 +294,58 @@ def opti_AR_GARCH_student(log_vraisemblance_GARCH_student,parametres_initiaux,ac
     print(f"Paramètres optimaux : {resultat_optimisation.x}")
     print(f"Succès de la convergence : {resultat_optimisation.success}")
     return phi0_opt, phi1_opt, alpha0_opt, alpha1_opt, beta0_opt, v_opt
+
+
+
+def simulate_GARCH_student(n, alpha0, alpha1, beta0,v):
+    if alpha0 <= 0 or alpha1 < 0 or beta0 < 0:
+        raise ValueError("Les paramètres doivent être strictement positifs pour garantir une variance > 0.")
+    if alpha1 + beta0 >= 1:
+        raise ValueError("Le processus n'est pas stationnaire (alpha1 + beta0 >= 1). La variance va exploser.")
+
+    rng = np.random.default_rng()
+    z = rng.standard_t(df=v, size=n)*np.sqrt((v-2)/v)
+    
+    eps = np.zeros(n)
+    sigma_carre = np.zeros(n)
+    
+    eps[0] = 0 #init
+    sigma_carre[0] = alpha0 / (1 - alpha1 - beta0) # Variance inconditionnelle du GARCH
+    
+    for k in range(1, n):
+        sigma_carre[k] = alpha0 + alpha1 * (eps[k-1]**2) + beta0 * sigma_carre[k-1]
+        
+        eps[k] = z[k] * np.sqrt(sigma_carre[k])
+        
+    return eps, sigma_carre
+
+    #sigma_carre est la vol conditionelle
+
+def AR_GARCH_student(n,phi0,phi1,alpha0,alpha1,beta0,v):
+    
+    if abs(phi1) >= 1:
+        raise ValueError("Paramètre AR invalide : le processus n'est pas stationnaire en moyenne.")
+    
+    if alpha0 <= 0 or alpha1 < 0 or beta0 < 0:
+        raise ValueError("Les paramètres doivent être strictement positifs pour garantir une variance > 0.")
+    if alpha1 + beta0 >= 1:
+        raise ValueError("Le processus n'est pas stationnaire (alpha1 + beta0 >= 1). La variance va exploser.")
+
+    epsilon, vol_conditionnelle = simulate_GARCH_student(n, alpha0, alpha1, beta0,v)
+
+    actif = np.zeros(n)
+
+    actif[0] = (phi0 / (1 - phi1)) + epsilon[0] #premier instant + choc
+
+    for k in range(1, n):
+        actif[k] = phi0 + phi1 * actif[k-1] + epsilon[k]
+
+    print(f'AR(1)-GARCH(1) with Zt suivant une loi de Student sur {n} périodes avec comme paramètres θ= (phi0 = {phi0}, phi1 = {phi1}, alpha0 = {alpha0}, alpha1 = {alpha1})')
+    plt.figure(figsize=(8, 5))
+    plt.plot(actif)
+    plt.title('AR(1)-GARCH(1)')
+    plt.xlabel('Temps')
+    plt.ylabel('Rt')
+    plt.show()
+
+    return epsilon,actif