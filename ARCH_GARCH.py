3e3c6224daf844f72cc7ea9e30398aac0071d0ea
Maxime-Skryma <maxime.skryma@gmail.com>
2026-08-29 18:41:02 +0000

    We fitted the log-yield

--- ARCH_GARCH.py
+++ ARCH_GARCH.py
@@ -147,8 +147,6 @@ def opti_AR_ARCH(log_vraisemblance,parametres_initiaux,actif):
     phi0_opt, phi1_opt, alpha0_opt, alpha1_opt = resultat_optimisation.x
     print(f"Paramètres optimaux : {resultat_optimisation.x}")
     print(f"Succès de la convergence : {resultat_optimisation.success}")
-    print(f"Les vrais paramètres sont phi0 = {phi0}, phi1 = {phi1}, alpha0 = {alpha0}, alpha1 = {alpha1}")
-
 
 def simulate_GARCH(n, alpha0, alpha1, beta0):
     if alpha0 <= 0 or alpha1 < 0 or beta0 < 0:
