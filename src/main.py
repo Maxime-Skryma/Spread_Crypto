0cc56783aa0a170d27591c322e6f7a14a42b0c32
Maxime-Skryma <maxime.skryma@gmail.com>
2026-09-16 16:13:40 +0000

    changes

--- src/main.py
+++ src/main.py
@@ -2,8 +2,15 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from model import fit_bivariate, bivariate_goodness_of_fit, compare_kernels_by_window_size
-from data import build_hawkes_dataframe, initialize_hawkes_params
+from model import (
+    fit_bivariate, bivariate_goodness_of_fit, 
+    fit_bivariate_sum_exp, bivariate_goodness_of_fit_sum_exp,
+    compare_kernels_by_window_size, qq_overlay
+)
+from data import (
+    build_hawkes_dataframe, initialize_hawkes_params,
+    initialize_hawkes_params_sum_exp
+)
 
 def run_real_data(
     raw_df,
@@ -30,7 +37,7 @@ def run_real_data(
         t_real = df_hawkes['time_stamp'].to_numpy()
         side_real = df_hawkes['side'].to_numpy()
 
-        # Fit Bivariate
+        # Fit (Mono-Exp)
         theta_init, bounds, constraints = initialize_hawkes_params(t_real, side_real)
         res_biv = fit_bivariate(
             df_hawkes, 
@@ -40,13 +47,32 @@ def run_real_data(
             verbose=False
         )
         
-        _, gof_biv = bivariate_goodness_of_fit(res_biv.x, df_hawkes, verbose=False)
+        # Residuals (bivariate)
+        (u1_mono, u2_mono), gof_biv = bivariate_goodness_of_fit(res_biv.x, df_hawkes, verbose=False)
         
-        f.write("--- Global Bivariate Fit (p-values) ---\n")
+        f.write("--- Global Bivariate Fit (Mono-Exp) ---\n")
         f.write(f"Dim 1 (Buy)  -> KS: {gof_biv[0][0]:.4f} | LB: {gof_biv[0][1]:.4f} | ED: {gof_biv[0][2]:.4f}\n")
         f.write(f"Dim 2 (Sell) -> KS: {gof_biv[1][0]:.4f} | LB: {gof_biv[1][1]:.4f} | ED: {gof_biv[1][2]:.4f}\n\n")
 
-        # Comparison between windows
+        # 3. Fit BivariateGlobal (Sum-Exp) and figures
+        theta_init_sum = initialize_hawkes_params_sum_exp(t_real, side_real)
+        res_sum = fit_bivariate_sum_exp(
+            df_hawkes, 
+            theta_init=theta_init_sum,
+            verbose=False
+        )
+        
+        (u1_sum, u2_sum), gof_sum = bivariate_goodness_of_fit_sum_exp(res_sum.x, df_hawkes, verbose=False)
+
+        f.write("--- Global Bivariate Fit (Sum-Exp) ---\n")
+        f.write(f"Dim 1 (Buy)  -> KS: {gof_sum[0][0]:.4f} | LB: {gof_sum[0][1]:.4f} | ED: {gof_sum[0][2]:.4f}\n")
+        f.write(f"Dim 2 (Sell) -> KS: {gof_sum[1][0]:.4f} | LB: {gof_sum[1][1]:.4f} | ED: {gof_sum[1][2]:.4f}\n\n")
+
+        # PNG
+        qq_overlay(u1_mono, u1_sum, title="Dimension 1 (Buys)")
+        qq_overlay(u2_mono, u2_sum, title="Dimension 2 (Sells)")
+
+        # Windows
         f.write("--- Window Comparison ---\n")
         f.write(f"{'Window':<10} | {'Model':<12} | {'Pass Rate':<10} | {'N Valid'}\n")
         f.write("-" * 50 + "\n")
@@ -69,10 +95,10 @@ def run_real_data(
     plt.show = original_show
 
 if __name__ == "__main__":
-
-    path="../data/binance_trades_2026-09-01_BTCUSDT.csv.gz"
-
-    df_btc = pd.read_csv(path)
+    
+    chemin = "../data/binance_trades_2026-09-01_BTCUSDT.csv.gz"
+    
+    df_btc = pd.read_csv(chemin)
     
     run_real_data(
         raw_df=df_btc, 
